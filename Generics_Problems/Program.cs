7cfe7eaa76a116321498c3ca790660d2d3b3dede
Madhuri <harshupatil0301@gmail>
2021-06-15 15:10:57 +0530

    [Madhuri]Add : Welcome Message Uploaded

---
+++ Generics_Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Generics Problems");
+        }
+    }
+}
